98ffe82a81729fd7515fda5c4bbf48b68c5f0513
piyush <pmimpiyush@gmail.com>
2024-06-04 19:00:09 +0530

    .

--- src/components/othersprofile.js
+++ src/components/othersprofile.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Link, useParams } from "react-router-dom";
-
+import loadinggif from '../Images/loading.gif';
 export default function Othersprofile() {
     const {_id} = useParams();
+      const [loading, setLoading] = useState(true);
+
   return (
-    <div className='h-[100vh] w-full bg-[red]'>
+    <div className='h-[100svh] w-full bg-[#000000] text-[#ededed]'>
+         {loading && (
+            <div className='absolute w-full inset-0 opacity-90 fill-neutral-950 z-[1000]'>
+              <div className='h-[30vh] bg-[#000000]'></div>
+              <img className='w-[100vw]' src={loadinggif} alt="Loading" />
+              <div className='h-[30vh] bg-[#000000]'></div>
+            </div>
+          )}
       sdgsdgds{_id}
     </div>
   )
