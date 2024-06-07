e1a793b763ac6667360fd0ae3454356eeaa67e7d
piyush <pmimpiyush@gmail.com>
2024-06-07 13:10:04 +0530

    made chat submit

--- src/components/chatpage.js
+++ src/components/chatpage.js
@@ -79,8 +79,8 @@ useEffect(()=>{
  },[chatarray2])
 
 
-  const handleChat = () => {
-
+  const handleChat = (e) => {
+e.preventDefault()
     const messageData = {
       teamname: teamnamein,
       userId: infoid,
@@ -132,17 +132,18 @@ useEffect(()=>{
             </div>
 
             <div className='h-[10svh]  bg-[#000000] flex justify-center items-center'>
-              <div className='w-[95%] bg-[#000000] h-full flex items-center'>
+              <form onsubmit={handleChat} className='w-[95%] bg-[#000000] h-full flex items-center'>
                 <input
                   placeholder='Start chatting'
                   value={msg}
                   onFocus={handleInputFocus} // Hide footer when input is focused
                   onBlur={() => setShowFooter(true)}
                   onChange={(e) => setmsg(e.target.value)}
+                  
                   className='bg-[#111112] pl-2 w-[90%] rounded-[10px] h-[75%] text-[#dadada] border-[#7a7a7a] rounded-r-[0px] border-[1px]'
                 />
-                <button className='p-2 pl-3 pr-3 rounded-[5px] h-[75%] border-[#dfdfdf] rounded-l-[0px] border-r-[1px] border-t-[1px] border-b-[1px] text-[#a8a8a8] font-mochiy-pop' onClick={handleChat}><IoIosSend /></button>
-              </div>
+                <button type='submit' className='p-2 pl-3 pr-3 rounded-[5px] h-[75%] border-[#dfdfdf] rounded-l-[0px] border-r-[1px] border-t-[1px] border-b-[1px] text-[#a8a8a8] font-mochiy-pop' onClick={handleChat}><IoIosSend /></button>
+              </form>
             </div>
           </div>
         </div>
